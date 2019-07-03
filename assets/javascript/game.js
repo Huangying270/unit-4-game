f101133d9d5d38230bf98e987240db62b041e512
Ying Huang <huangying270@gmail.com>
2019-07-03 17:59:50 -0400

    linked html to css and js, pseudo coded in javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// 4 crystal variables
+// each crystal will generate a random number between 1-12
+// random number will be shown between 19-120 at the start of the game
+// on click crystal, a number will be added to previous
+// number added will be hidden until crystal is clicked
+// total must match random number to win and lose if over
+// game will restart after
+// new game new random number, new random crystal numbers
+// keep track of number of games won and loss
